9724b7780b8ecf7f23c13578c0054561ad2017ea
Balatom <m.balamuruganmdu5@gmail.com>
2022-07-10 23:07:53 +0530

    3 files modified, 5 added, 3 deleted

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 //setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath));
