5311675d6a68aea74ada2d6b1a030c3a2a245009
Balatom <m.balamuruganmdu5@gmail.com>
2022-07-10 18:26:27 +0530

    5 files modified, 1 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const app = express();
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
+app.set('view engine', 'hbs');
+
+app.get('', (req, res) => {
+  res.render('index');
+});
 
 app.get('/weather', (req, res) => {
   res.send({
